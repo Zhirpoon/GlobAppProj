9b4050e721eb5ab4db5bca00c3d2828f6565f3cd
jeanneDarc87 <Bertil@DESKTOP-F3CF4M0>
2018-01-31 14:35:56 +0100

    created package layout with 1 dummy class in each layer

--- src/main/java/se/kth/id1212/globalapps/dtos/RegistrationDTO.java
+++ src/main/java/se/kth/id1212/globalapps/dtos/RegistrationDTO.java
@@ -4,7 +4,7 @@ package se.kth.id1212.globalapps.dtos;
  *
  * @author Diaco Uthman
  */
-public interface RegistrationDTO {
+public interface RegistrationDTO extends LoginCredentialsDTO {
     public String getFirstname();
     public String getLastname();
     public String getMail();
