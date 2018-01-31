d33586219f78234ab8103bb4454710786c921be2
FlyHighXX <diacouthman@gmail.com>
2018-01-31 14:43:48 +0100

    DBAO created with empty methods

--- src/main/java/se/kth/id1212/globalapps/integration/DBAO.java
+++ src/main/java/se/kth/id1212/globalapps/integration/DBAO.java
@@ -1,9 +1,42 @@
 package se.kth.id1212.globalapps.integration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author Anders Klasson <aklasson@kth.se>
  */
 public class DBAO {
+    private final EntityManagerFactory emFactory;
+    private final ThreadLocal<EntityManager> threadEM = new ThreadLocal<>();
+    
+    public DBAO(){
+        this.emFactory = Persistence.createEntityManagerFactory("GlobalAppPU");
+    }
+    
+    public void addUser(UserEntity user){
+    }
+    
+    public UserEntity getUser(){
+    }
+    
+    public String[] getAllExpertises(){
+    }
+    
+    public void addApplication(ApplicationEntity application){
+    }
+    
+    public ApplicationEntity[] getApplicationByCriteria(ApplicationSearchDTO searchCriterias){
+    }
+    
+    public void updateApplicationStatusRejected(){
+    }
+    
+    public void updateApplicationStatusAccepted(){
+    }
+    
+    
     
 }
